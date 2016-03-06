397fd2e2c091fbab1388e3fc6a9bcafa1afb98fa
Jimmy Tang <jcftang@gmail.com>
2016-03-06 18:40:59 +0000

    Update ansible to latest stable

--- config/software/ansible.rb
+++ config/software/ansible.rb
@@ -1,5 +1,8 @@
 name "ansible"
-default_version "release1.8.4"
+default_version "2.0.1.0"
+
+version "1.8.4"
+version "2.0.1.0"
 
 dependency "bzip2"
 dependency "gdbm"
@@ -12,6 +15,6 @@ dependency "virtualenv"
 
 build do
   command "#{install_dir}/embedded/bin/virtualenv #{install_dir}/embedded/ansible"
-  command "#{install_dir}/embedded/ansible/bin/pip install ansible"
+  command "#{install_dir}/embedded/ansible/bin/pip install ansible==#{version}"
   command "cp #{install_dir}/embedded/ansible/bin/ansible{,-doc,-playbook,-pull} #{install_dir}/bin"
 end
